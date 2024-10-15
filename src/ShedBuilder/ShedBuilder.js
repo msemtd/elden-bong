86ba3964953abadc3b8d8cc5d3ff0583fcc3ddfe
Michael Erskine <michael.erskine@faro.com>
2024-10-15 15:11:34 +0100

    components for shed building

--- src/ShedBuilder/ShedBuilder.js
+++ src/ShedBuilder/ShedBuilder.js
@@ -12,6 +12,7 @@ class ShedBuilder extends THREE.EventDispatcher {
     this.gui = null
     this.group = new THREE.Group()
     this.group.name = 'ShedBuilder'
+    this.addComponents()
     // -------------------------------------
 
     // ------------------------------------------------
@@ -65,6 +66,44 @@ class ShedBuilder extends THREE.EventDispatcher {
     mesh.position.set(5, 8, depth / 2)
     mesh.name = shedName
     this.group.add(mesh)
+
+    const lap = this.components.shipLap.clone()
+    this.group.add(lap)
+    lap.position.set(5, 5, 5)
+    lap.rotateX(-Math.PI / 2.0)
+    lap.rotateY(Math.PI / 2.0)
+    this.redraw()
+  }
+
+  addComponents () {
+    const gp = new THREE.BoxGeometry(2.1, 0.075, 0.075)
+    const mp = new THREE.MeshLambertMaterial({ color: 'green' })
+    const post = new THREE.Mesh(gp, mp)
+    // ship lap could be a profile!
+    const profile = new THREE.Shape()
+    const thickness = 14.5 / 1000
+    const tw = thickness / 2.0
+    const lap = 9 / 1000
+    const height = 119 / 1000
+    profile.lineTo(tw, 0)
+    profile.lineTo(tw, lap * 1.3)
+    profile.lineTo(thickness, lap * 3)
+    profile.lineTo(thickness, height)
+    profile.lineTo(tw, height)
+    profile.lineTo(tw, height - lap)
+    profile.lineTo(0, height - lap)
+    profile.lineTo(0, 0)
+
+    const geometry = new THREE.ExtrudeGeometry(profile, { depth: 2.4, bevelEnabled: false })
+    const material = new THREE.MeshPhongMaterial({ color: 'tan' })
+    const shipLap = new THREE.Mesh(geometry, material)
+    const link = 'https://www.wickes.co.uk/Wickes-Treated-Rebated-Shiplap-14-5-x-119-x-2400mm/p/9000281526'
+    shipLap.userData = { link, pricePerUnit: '£9.40', overlap: 0.110 }
+
+    const b = new THREE.BoxGeometry(0.0145, 0.119, 2.4)
+    const m = new THREE.MeshLambertMaterial({ color: 'brown' })
+    const plank = new THREE.Mesh(b, m)
+    this.components = { post, shipLap, plank }
   }
 }
 
