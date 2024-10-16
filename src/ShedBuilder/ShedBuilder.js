464a9c05bb7efae9ee3d8e2b26711a8f33d64143
Michael Erskine <michael.erskine@faro.com>
2024-10-16 15:28:01 +0100

    rotate shed posts

--- src/ShedBuilder/ShedBuilder.js
+++ src/ShedBuilder/ShedBuilder.js
@@ -50,6 +50,68 @@ class ShedBuilder extends THREE.EventDispatcher {
     }
   }
 
+  addComponents () {
+    let post = null
+    let shipLap = null
+    let plank = null
+    {
+      const b = new THREE.BoxGeometry(0.0145, 0.119, 2.4)
+      const m = new THREE.MeshLambertMaterial({ color: 'brown' })
+      plank = new THREE.Mesh(b, m)
+    }
+    {
+      const length = 2.1
+      const crossSection = 0.075
+      const gp = new THREE.BoxGeometry(crossSection, crossSection, length)
+      // adjust origin
+      gp.translate(crossSection / 2, crossSection / 2, length / 2)
+      const mp = new THREE.MeshLambertMaterial({ color: 'green' })
+      post = new THREE.Mesh(gp, mp)
+      post.userData = {
+        length,
+        crossSection,
+        pricePerUnit: '£13.00',
+        link: 'https://www.wickes.co.uk/Incised-Timber-Fence-Post---75-x-75-x-2100mm/p/542002'
+      }
+    }
+    {
+    // ship-lap profile...
+      const profile = new THREE.Shape()
+      const thickness = 14.5 / 1000
+      const tw = thickness / 2.0
+      const lap = 9 / 1000
+      const height = 119 / 1000
+      const length = 2.4
+      // draw the profile from bottom left...
+      profile.moveTo(tw, 0)
+      profile.lineTo(thickness, 0)
+      profile.lineTo(thickness, height - (lap * 3))
+      profile.lineTo(tw, height - (lap * 1.3))
+      profile.lineTo(tw, height)
+      profile.lineTo(0, height)
+      profile.lineTo(0, lap)
+      profile.lineTo(tw, lap)
+      profile.lineTo(tw, 0)
+      // extrude into 3D geometry and adjust to expected orientation -
+      // a horizontal board with origin at bottom left of back face
+      const geometry = new THREE.ExtrudeGeometry(profile, { depth: length, bevelEnabled: false })
+      geometry.rotateZ(-Math.PI / 2)
+      geometry.rotateY(-Math.PI / 2)
+      geometry.translate(length, 0, 0)
+      const material = new THREE.MeshPhongMaterial({ color: 'tan' })
+      shipLap = new THREE.Mesh(geometry, material)
+      shipLap.userData = {
+        link: 'https://www.wickes.co.uk/Wickes-Treated-Rebated-Shiplap-14-5-x-119-x-2400mm/p/9000281526',
+        pricePerUnit: '£9.40',
+        overlap: 0.112,
+        height,
+        length,
+        thickness,
+      }
+    }
+    this.components = { post, shipLap, plank }
+  }
+
   makeShed (shedName) {
     const width = 4
     const height = 3
@@ -89,54 +151,23 @@ class ShedBuilder extends THREE.EventDispatcher {
         this.group.add(o)
       }
     }
-    this.redraw()
-  }
 
-  addComponents () {
-    const b = new THREE.BoxGeometry(0.0145, 0.119, 2.4)
-    const m = new THREE.MeshLambertMaterial({ color: 'brown' })
-    const plank = new THREE.Mesh(b, m)
-    const gp = new THREE.BoxGeometry(2.1, 0.075, 0.075)
-    const mp = new THREE.MeshLambertMaterial({ color: 'green' })
-    const post = new THREE.Mesh(gp, mp)
-    post.userData = {
-      pricePerUnit: '£13.00',
-      link: 'https://www.wickes.co.uk/Incised-Timber-Fence-Post---75-x-75-x-2100mm/p/542002'
+    {
+      const c = this.components.post
+      console.assert(isObject(c))
+      console.assert(c?.isMesh)
+      const postPositions = [[0, 0, 0], [1, 0, 0], [1, 1, 0], [0, 1, 0]]
+      for (let i = 0; i < postPositions.length; i++) {
+        const e = postPositions[i]
+        const o = c.clone()
+        p.set(e[0], e[1], e[2])
+        o.position.copy(p)
+        o.rotateZ(i * Math.PI / 2)
+        this.group.add(o)
+      }
     }
-    // ship-lap profile...
-    const profile = new THREE.Shape()
-    const thickness = 14.5 / 1000
-    const tw = thickness / 2.0
-    const lap = 9 / 1000
-    const height = 119 / 1000
-    const length = 2.4
-    // draw the profile from bottom left...
-    profile.moveTo(tw, 0)
-    profile.lineTo(thickness, 0)
-    profile.lineTo(thickness, height - (lap * 3))
-    profile.lineTo(tw, height - (lap * 1.3))
-    profile.lineTo(tw, height)
-    profile.lineTo(0, height)
-    profile.lineTo(0, lap)
-    profile.lineTo(tw, lap)
-    profile.lineTo(tw, 0)
-    // extrude into 3D geometry and adjust to expected orientation -
-    // a horizontal board with origin at bottom left of back face
-    const geometry = new THREE.ExtrudeGeometry(profile, { depth: length, bevelEnabled: false })
-    geometry.rotateZ(-Math.PI / 2)
-    geometry.rotateY(-Math.PI / 2)
-    geometry.translate(length, 0, 0)
-    const material = new THREE.MeshPhongMaterial({ color: 'tan' })
-    const shipLap = new THREE.Mesh(geometry, material)
-    shipLap.userData = {
-      link: 'https://www.wickes.co.uk/Wickes-Treated-Rebated-Shiplap-14-5-x-119-x-2400mm/p/9000281526',
-      pricePerUnit: '£9.40',
-      overlap: 0.112,
-      height,
-      length,
-      thickness,
-    }
-    this.components = { post, shipLap, plank }
+
+    this.redraw()
   }
 }
 
