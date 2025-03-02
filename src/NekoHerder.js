3fbf8820e66cd0f828725a7a9e1a1fd536a3cd50
Michael Erskine <msemtd@googlemail.com>
2025-03-02 20:43:33 +0000

    neko herder mini-game idea

---
+++ src/NekoHerder.js
@@ -0,0 +1,10 @@
+import { MiniGameBase } from './MiniGameBase'
+
+class NekoHerder extends MiniGameBase {
+  comstructor () {
+    // https://hitoikigame.com/blog-entry-7879.html
+    // just wraps https://llerrah.com/cattrapgame1.htm ???
+    // 11 x 11 hexagonal grid - cat begins in the middle
+    // how many grid places get randomly set?
+  }
+}
