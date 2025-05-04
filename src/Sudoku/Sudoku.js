0ccd18cf4f75b1356e11fb630af11f5eef916b49
Michael Erskine <msemtd@googlemail.com>
2025-05-04 16:58:51 +0100

    sudoku board model

--- src/Sudoku/Sudoku.js
+++ src/Sudoku/Sudoku.js
@@ -1,9 +1,10 @@
 import { MiniGameBase } from '../MiniGameBase.js'
 import * as THREE from 'three'
-import { generalObj3dClean } from '../threeUtil'
+import { generalObj3dClean, depthFirstReverseTraverse } from '../threeUtil'
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js'
 import { Screen } from '../Screen'
 import CameraControls from 'camera-controls'
+import * as sudoku from 'sudoku'
 
 /*
   https://en.wikipedia.org/wiki/Sudoku
@@ -15,7 +16,6 @@ import CameraControls from 'camera-controls'
 
   solving room? - just first-person view looking at a pad of paper with a pencil and eraser
 
-
 */
 
 export class Sudoku extends MiniGameBase {
@@ -26,6 +26,10 @@ export class Sudoku extends MiniGameBase {
       console.assert(this.gui instanceof GUI)
       console.assert(this.group instanceof THREE.Group)
       this.gui.add(this, 'runTest')
+      this.gui.open()
+      setTimeout(() => {
+        this.runTest()
+      }, 600)
     })
   }
 
@@ -43,6 +47,54 @@ export class Sudoku extends MiniGameBase {
     `
     const dat = this.parseBoard(brd)
     console.dir(dat)
+
+    const puzzle = sudoku.makepuzzle()
+    console.dir(puzzle)
+
+    depthFirstReverseTraverse(null, this.group, generalObj3dClean)
+    // let's make a board
+    const grp = this.group
+    const g = new THREE.BoxGeometry(10, 10, 1, 9, 9, 1)
+    const e = new THREE.EdgesGeometry(g)
+    const m = new THREE.MeshLambertMaterial({
+      color: 'gainsboro',
+      polygonOffset: true,
+      polygonOffsetFactor: 1,
+      polygonOffsetUnits: 1
+    })
+    const em = new THREE.LineBasicMaterial({ color: 'black' })
+    const o = new THREE.Mesh(g, m)
+    const objectEdges = new THREE.LineSegments(e, em)
+    o.add(objectEdges)
+    o.position.set(4, 4, -0.6)
+    grp.add(o)
+    // board parts...
+    const squareGeometry = new THREE.BoxGeometry(1, 1, 0.05)
+    const squareMaterial = new THREE.MeshLambertMaterial({ color: 'pink' })
+    const barGeometry = new THREE.BoxGeometry(0.05, 0.05, 9)
+    const barMaterial = new THREE.MeshLambertMaterial({ color: 'brown' })
+    for (let c = 0; c <= 9; c++) {
+      const vBar = new THREE.Mesh(barGeometry, barMaterial)
+      vBar.position.set(c - 0.5, 4, 0.03)
+      vBar.rotateX(Math.PI / 2)
+      grp.add(vBar)
+      for (let r = 0; r <= 9; r++) {
+        if (r < 9 && c < 9) {
+          const square = new THREE.Mesh(squareGeometry, squareMaterial)
+          square.name = `square_${r}_${c}`
+          square.position.set(c, r, 0.01)
+          grp.add(square)
+        }
+        if (c === 0) {
+          const hBar = new THREE.Mesh(barGeometry, barMaterial)
+          hBar.position.set(4, r - 0.5, 0.03)
+          hBar.rotateY(Math.PI / 2)
+          grp.add(hBar)
+        }
+      }
+    }
+
+    this.activate()
   }
 
   parseBoard (brd) {
