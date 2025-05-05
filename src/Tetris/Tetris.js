3d8b9252d956dbd039a642f7c9878163109d336f
Michael Erskine <msemtd@googlemail.com>
2025-05-05 21:31:08 +0100

    add tetris for laughs

---
+++ src/Tetris/Tetris.js
@@ -0,0 +1,27 @@
+import { MiniGameBase } from '../MiniGameBase'
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js'
+import { Screen } from '../Screen'
+import CameraControls from 'camera-controls'
+import * as THREE from 'three'
+import { generalObj3dClean, depthFirstReverseTraverse } from '../threeUtil'
+
+export class Tetris extends MiniGameBase {
+  constructor (parent) {
+    super(parent, 'Tetris')
+    parent.addEventListener('ready', (ev) => {
+      this.onReady(ev)
+      console.assert(this.gui instanceof GUI)
+      console.assert(this.group instanceof THREE.Group)
+      this.gui.add(this, 'runTest')
+      this.gui.open()
+    })
+  }
+
+  runTest () {
+    // what to do here? Draw a playing field?
+    // define the blocks - get the colours right!
+    // define the game
+    // the controls
+    // all dat stuff!
+  }
+}
