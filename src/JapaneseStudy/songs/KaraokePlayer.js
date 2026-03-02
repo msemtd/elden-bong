20fdd897d4af90063df774235aacdc0c50112db6
Michael Erskine <michael.erskine@faro.com>
2026-03-02 16:19:09 +0000

    scg icons to use

--- src/JapaneseStudy/songs/KaraokePlayer.js
+++ src/JapaneseStudy/songs/KaraokePlayer.js
@@ -79,6 +79,18 @@ export class KaraokePlayer extends MiniGameBase {
         p({ class: 'lineNext' }, lineNext),
       ),
       // <!-- Controls -->
+
+      /*
+https://icones.js.org/collection/flowbite
+<symbol viewBox="0 0 24 24" id="flowbite-backward-step-outline"><!-- Icon from Flowbite Icons by Themesberg - https://github.com/themesberg/flowbite-icons/blob/main/LICENSE --><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 6v12m8-12v12l-8-6z"></path></symbol>
+<symbol viewBox="0 0 24 24" id="flowbite-play-outline"><!-- Icon from Flowbite Icons by Themesberg - https://github.com/themesberg/flowbite-icons/blob/main/LICENSE --><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 18V6l8 6z"></path></symbol>
+<symbol viewBox="0 0 24 24" id="flowbite-pause-outline"><!-- Icon from Flowbite Icons by Themesberg - https://github.com/themesberg/flowbite-icons/blob/main/LICENSE --><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 6H8a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1m7 0h-1a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1"></path></symbol>
+<symbol viewBox="0 0 24 24" id="flowbite-stop-outline"><!-- Icon from Flowbite Icons by Themesberg - https://github.com/themesberg/flowbite-icons/blob/main/LICENSE --><rect width="12" height="12" x="6" y="6" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2" rx="1"></rect></symbol>
+<symbol viewBox="0 0 24 24" id="flowbite-forward-step-outline"><!-- Icon from Flowbite Icons by Themesberg - https://github.com/themesberg/flowbite-icons/blob/main/LICENSE --><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 6v12M8 6v12l8-6z"></path></symbol>
+<symbol viewBox="0 0 24 24" id="flowbite-folder-open-outline"><!-- Icon from Flowbite Icons by Themesberg - https://github.com/themesberg/flowbite-icons/blob/main/LICENSE --><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 19V6a1 1 0 0 1 1-1h4.032a1 1 0 0 1 .768.36l1.9 2.28a1 1 0 0 0 .768.36H16a1 1 0 0 1 1 1v1M3 19l3-8h15l-3 8z"></path></symbol>
+<symbol viewBox="0 0 24 24" id="flowbite-list-outline"><!-- Icon from Flowbite Icons by Themesberg - https://github.com/themesberg/flowbite-icons/blob/main/LICENSE --><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M9 8h10M9 12h10M9 16h10M4.99 8H5m-.02 4h.01m0 4H5"></path></symbol>
+<symbol viewBox="0 0 24 24" id="flowbite-volume-up-outline"><!-- Icon from Flowbite Icons by Themesberg - https://github.com/themesberg/flowbite-icons/blob/main/LICENSE --><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.5 8.43A5 5 0 0 1 17 12a4.98 4.98 0 0 1-1.43 3.5m2.794 2.864A8.97 8.97 0 0 0 21 12a8.97 8.97 0 0 0-2.636-6.364M12 6.135v11.73a1 1 0 0 1-1.64.768L6 15H4a1 1 0 0 1-1-1v-4a1 1 0 0 1 1-1h2l4.36-3.633a1 1 0 0 1 1.64.768"></path></symbol>
+      */
       div({ class: 'controlsOuter' },
         div({ class: 'controlsInner' },
           button({ class: 'prevBtn' }, 'previous'),
@@ -86,7 +98,6 @@ export class KaraokePlayer extends MiniGameBase {
           button({ class: 'pauseBtn' }, 'pause'),
           button({ class: 'stopBtn' }, 'stop'),
           button({ class: 'nextBtn' }, 'next'),
-          button({ class: 'playBtn' }, 'play'),
           button({ class: 'ejectBtn' }, 'open'),
         ),
         button({ class: 'playlistBtn' }, 'playlist'),
