408c3a4f9bf4325da554cdb42f28f2686121c1a0
Michael Erskine <michael.erskine@faro.com>
2026-04-17 15:50:45 +0100

    roumaji for Tokyo Bus Girl

--- src/JapaneseStudy/songs/TokyoBasuGaru.js
+++ src/JapaneseStudy/songs/TokyoBasuGaru.js
@@ -7,44 +7,64 @@ export class TokyoBasuGaru extends SongBase {
   constructor () {
     super('東京のバスガール', 'コロムビア・ローズ', 1957)
     const a = AboutSongs.layouts
-    this.dataLayout = [a.KANJI]
+    this.dataLayout = [a.KANJI, a.ROMAJI]
     this.data = `
 
-若い希望も
-恋もある
-ビルの街から
-山の手へ
-紺の制服
-身につけて
-私は東京の
-バスガール
-『発車 オーライ』
-明るく明るく
-走るのよ
+若い希望も恋もある
+Wakai kibō mo koi mo aru
 
-昨日心に
-とめた方
-今日はきれいな
-人つれて
-夢ははかなく
-破れても
-くじけちゃいけない
-バスガール
-『発車 オーライ』
-明るく明るく
-走るのよ
+ビルの街から山の手へ
+Biru no machi kara Yamanote e
 
-酔ったお客の
-意地悪さ
-いやな言葉で
-どなられて
-ほろり落した
-ひとしずく
-それでも東京の
-バスガール
+紺の制服身につけて
+Kon no seifuku mi ni tsukete
+
+私は東京のバスガール
+Watashi wa Tōkyō no basu ga-ru
+
 『発車 オーライ』
-明るく明るく
-走るのよ
+"hassha o- rai"
+
+明るく明るく走るのよ
+Akaruku akaruku hashiru no yo
+
+昨日心にとめた方
+Kinō kokoro ni tometa hō
+
+今日はきれいな人つれて
+Kyō wa kireina hito tsurete
+
+夢ははかなく破れても
+Yume wa hakanaku yaburete mo
+
+くじけちゃいけないバスガール
+Kujike chaikenai basu ga-ru
+
+『発車 オーライ』
+"hassha o- rai"
+
+明るく明るく走るのよ
+Akaruku akaruku hashiru no yo
+
+
+酔ったお客の意地悪さ
+Yotta okyaku no ijiwarusa
+
+いやな言葉でどなられて
+Iyana kotoba de donararete
+
+ほろり落したひとしずく
+Horori otoshita hito shizuku
+
+それでも東京のバスガール
+Soredemo Tōkyō no basu ga-ru
+
+"hassha o- rai"
+『発車 オーライ』
+
+明るく明るく走るのよ
+Akaruku akaruku hashiru no yo
+
 
 `.trim()
     // cspell: enable
