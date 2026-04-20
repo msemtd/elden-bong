c3c82b303950222baf0055d1363c2e90b8465550
Michael Erskine <msemtd@googlemail.com>
2026-04-20 20:44:07 +0100

    chords

--- src/JapaneseStudy/songs/SoreGaDaiji.js
+++ src/JapaneseStudy/songs/SoreGaDaiji.js
@@ -4,6 +4,9 @@ import { AboutSongs, SongBase } from './AboutSongs'
 export class SoreGaDaiji extends SongBase {
   constructor () {
     super('それが大事 (Sore Ga Daiji)', 'DaijiMAN Brothers Band', 2007)
+    this.video = 'https://www.youtube.com/watch?v=aCMt0H31msk'
+    this.music = 'https://www.hooktheory.com/theorytab/view/daijiman-brothers-band/sore-ga-daiji'
+
     const a = AboutSongs.layouts
     this.dataLayout = [a.ROMAJI, a.KANJI, a.ENGLISH]
     this.data = `
